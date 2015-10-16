77c2b9b5fff1598ea9978086564f18a3995c36e7
Aidan Delaney <aidan@ontologyengineering.org>
2015-10-16 10:10:23 +0100

    Configure Spark to pick up OPENSHIFT vars if they're available.

--- src/main/java/org/eulerdiagrams/euleR/Main.java
+++ src/main/java/org/eulerdiagrams/euleR/Main.java
@@ -3,7 +3,13 @@ package org.eulerdiagrams.euleR;
 import static spark.Spark.*;
 
 public class Main {
+    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
+
     public static void main(String[] args) {
+        ipAddress(IP_ADDRESS);
+        port(PORT);
+        staticFileLocation("/public");
         get("/hello", (req, res) -> "Hello World");
     }
 }
