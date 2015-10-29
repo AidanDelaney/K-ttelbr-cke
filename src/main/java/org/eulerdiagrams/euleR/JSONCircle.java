53b8d65c820299c57a34bc8c1d4fc5d17378c8ec
Aidan Delaney <aidan@ontologyengineering.org>
2015-10-29 15:04:05 +0000

    Simplify circle for easier JSON deserialisation (from calling code)

--- src/main/java/org/eulerdiagrams/euleR/JSONCircle.java
+++ src/main/java/org/eulerdiagrams/euleR/JSONCircle.java
@@ -3,11 +3,13 @@ package org.eulerdiagrams.euleR;
 import math.geom2d.conic.Circle2D;
 
 public class JSONCircle {
-    public Circle2D circle;
+    public double x, y, radius;
     public String label;
 
     public JSONCircle(Circle2D circle, String label) {
-        this.circle = circle;
+        this.x = circle.center().x();
+        this.y = circle.center().y();
+        this.radius = circle.radius();
         this.label = label;
     }
 }
