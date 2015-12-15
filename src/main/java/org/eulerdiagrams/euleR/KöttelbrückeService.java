565da82c15b4cb1d9c7ad01d1d5067274eb3c664
Aidan Delaney <aidan@ontologyengineering.org>
2015-12-15 11:25:31 +0000

    Moved static setup into the service implementation.

--- src/main/java/org/eulerdiagrams/euleR/KöttelbrückeService.java
+++ src/main/java/org/eulerdiagrams/euleR/KöttelbrückeService.java
@@ -1,20 +1,72 @@
 package org.eulerdiagrams.euleR;
 
-import java.awt.Point;
+import java.awt.*;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 import org.eulerdiagrams.AbstractDiagram.AbstractDiagram;
 import org.eulerdiagrams.vennom.apCircles.VennomLayout;
+import org.eulerdiagrams.vennom.graph.EdgeType;
 import org.eulerdiagrams.vennom.graph.Graph;
 import org.eulerdiagrams.vennom.graph.Node;
+
+import static org.eulerdiagrams.vennom.apCircles.display.APCircleDisplay.*;
 
 import com.google.common.base.Stopwatch;
 
 import math.geom2d.conic.Circle2D;
 
 public class KöttelbrückeService {
+    static {
+        /*
+         * The lines until the <<< marker are junk that is necessary to set before you can use the VenNom graph drawer.
+         * It's not pretty, but without setting properties of the EdgeTypes you get NullPointerExceptions from the
+         * VenNom code.
+         */
+        FIXED = new EdgeType("fixed");
+        FIXED.setLineColor(Color.black);
+        FIXED.setTextColor(Color.black);
+        FIXED.setSelectedLineColor(Color.gray);
+        FIXED.setPriority(1020);
+
+        ATTRACTOR = new EdgeType("attractor");
+        ATTRACTOR.setLineColor(Color.green);
+        ATTRACTOR.setTextColor(Color.green);
+        ATTRACTOR.setSelectedLineColor(Color.gray);
+        ATTRACTOR.setPriority(1019);
+
+        REPULSOR = new EdgeType("repulsor");
+        REPULSOR.setLineColor(Color.red);
+        REPULSOR.setTextColor(Color.red);
+        REPULSOR.setSelectedLineColor(Color.gray);
+        REPULSOR.setPriority(1018);
+
+        SEPARATOR = new EdgeType("separator");
+        SEPARATOR.setLineColor(Color.cyan);
+        SEPARATOR.setTextColor(Color.cyan);
+        SEPARATOR.setSelectedLineColor(Color.gray);
+        SEPARATOR.setPriority(1017);
+
+        IDEAL = new EdgeType("ideal");
+        IDEAL.setLineColor(Color.blue);
+        IDEAL.setTextColor(Color.blue);
+        IDEAL.setSelectedLineColor(Color.gray);
+        IDEAL.setPriority(1016);
+
+        CONTAINMENT = new EdgeType("containment");
+        CONTAINMENT.setLineColor(Color.magenta);
+        CONTAINMENT.setTextColor(Color.magenta);
+        CONTAINMENT.setSelectedLineColor(Color.magenta);
+        CONTAINMENT.setPriority(1016);
+
+        Graph.DEFAULT_NODE_TYPE.setHeight(20);
+        Graph.DEFAULT_NODE_TYPE.setWidth(20);
+        Graph.DEFAULT_NODE_TYPE.setBorderColor(Color.WHITE);
+        Graph.DEFAULT_NODE_TYPE.setTextColor(Color.BLUE);
+        Graph.DEFAULT_EDGE_TYPE = IDEAL;
+        // <<<
+    }
     private List<JSONCircle> circles;
     private Graph graph;
     private Stopwatch timer;
@@ -23,8 +75,8 @@ public class KöttelbrückeService {
     public KöttelbrückeService(JSONArea areaSpec) {
         adp = new AbstractDiagramProvider(areaSpec);
         VennomLayout vl = new VennomLayout(VennomLayout.FORCE_LAYOUT, adp.asAreaSpecification());
-        graph = vl.layout();
         timer = Stopwatch.createStarted();
+        graph = vl.layout();
         timer.stop();
         circles = graphToConcreteCircles(graph);
     }
@@ -49,7 +101,7 @@ public class KöttelbrückeService {
         List<JSONCircle> circles = new Vector<>();
 
         for(Node n: graph.getNodes()) {
-            Point centre = n.getCentre();
+            Point.Double centre = n.getPreciseCentre();
             circles.add(new JSONCircle(new Circle2D(centre.getX(), centre.getY(), n.getPreciseRadius()), n.getContour()));
         }
         return circles;
