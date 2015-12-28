79b848e0683e12954455e86e6ad76e33e9be5654
Aidan Delaney <aidan@ontologyengineering.org>
2015-12-28 11:18:24 +0000

    Static block no longer necessary due to improvements in Vennom.

--- src/main/java/org/eulerdiagrams/euleR/KöttelbrückeService.java
+++ src/main/java/org/eulerdiagrams/euleR/KöttelbrückeService.java
@@ -18,55 +18,7 @@ import com.google.common.base.Stopwatch;
 import math.geom2d.conic.Circle2D;
 
 public class KöttelbrückeService {
-    static {
-        /*
-         * The lines until the <<< marker are junk that is necessary to set before you can use the VenNom graph drawer.
-         * It's not pretty, but without setting properties of the EdgeTypes you get NullPointerExceptions from the
-         * VenNom code.
-         */
-        EdgeType FIXED = new EdgeType("fixed");
-        FIXED.setLineColor(Color.black);
-        FIXED.setTextColor(Color.black);
-        FIXED.setSelectedLineColor(Color.gray);
-        FIXED.setPriority(1020);
-
-        EdgeType ATTRACTOR = new EdgeType("attractor");
-        ATTRACTOR.setLineColor(Color.green);
-        ATTRACTOR.setTextColor(Color.green);
-        ATTRACTOR.setSelectedLineColor(Color.gray);
-        ATTRACTOR.setPriority(1019);
-
-        EdgeType REPULSOR = new EdgeType("repulsor");
-        REPULSOR.setLineColor(Color.red);
-        REPULSOR.setTextColor(Color.red);
-        REPULSOR.setSelectedLineColor(Color.gray);
-        REPULSOR.setPriority(1018);
-
-        EdgeType SEPARATOR = new EdgeType("separator");
-        SEPARATOR.setLineColor(Color.cyan);
-        SEPARATOR.setTextColor(Color.cyan);
-        SEPARATOR.setSelectedLineColor(Color.gray);
-        SEPARATOR.setPriority(1017);
-
-        EdgeType IDEAL = new EdgeType("ideal");
-        IDEAL.setLineColor(Color.blue);
-        IDEAL.setTextColor(Color.blue);
-        IDEAL.setSelectedLineColor(Color.gray);
-        IDEAL.setPriority(1016);
-
-        EdgeType CONTAINMENT = new EdgeType("containment");
-        CONTAINMENT.setLineColor(Color.magenta);
-        CONTAINMENT.setTextColor(Color.magenta);
-        CONTAINMENT.setSelectedLineColor(Color.magenta);
-        CONTAINMENT.setPriority(1016);
-
-        Graph.DEFAULT_NODE_TYPE.setHeight(20);
-        Graph.DEFAULT_NODE_TYPE.setWidth(20);
-        Graph.DEFAULT_NODE_TYPE.setBorderColor(Color.WHITE);
-        Graph.DEFAULT_NODE_TYPE.setTextColor(Color.BLUE);
-        Graph.DEFAULT_EDGE_TYPE = IDEAL;
-        // <<<
-    }
+   
     private List<JSONCircle> circles;
     private Graph graph;
     private Stopwatch timer;
